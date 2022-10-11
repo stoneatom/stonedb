973fac9ecace8ccbed6055e862e7bb47dee758c6
lihongjian <lihongjian@stoneatom.com>
2022-10-11 09:21:34 +0800

    fix(tianmu) explain select straight_join DISTINCT.The rows in the test result is incorrect (#270) Cause of the problem: In the mode where the type is ref, the line number returned by the tianmu engine is 10 by default. Modify Scheme: Delete the logic that returns 10 rows by default, and return the number of rows in the table instead.

--- storage/tianmu/handler/tianmu_handler.cpp
+++ storage/tianmu/handler/tianmu_handler.cpp
@@ -1203,7 +1203,7 @@ int TianmuHandler::delete_table(const char *name) {
 ha_rows TianmuHandler::records_in_range([[maybe_unused]] uint inx, [[maybe_unused]] key_range *min_key,
                                         [[maybe_unused]] key_range *max_key) {
   DBUG_ENTER(__PRETTY_FUNCTION__);
-  DBUG_RETURN(10);  // low number to force index usage
+  DBUG_RETURN(stats.records);  // low number to force index usage
 }
 
 /*
