3b82eab43a3d264c0d29a63e0947c9fd2ce8ba02
RingsC <hom.lee@hotmail.com>
2022-10-13 15:22:11 +0800

    feat(tianmu): To support ARM arch (#696).
    Enable compiling and running StoneDB on ARM arch, such as ARM server, on Apple.

--- storage/tianmu/base/core/cacheline.h
+++ storage/tianmu/base/core/cacheline.h
@@ -36,6 +36,10 @@ static constexpr size_t cache_line_size =
     256;
 #elif defined(__ppc64__)
     128;
+#elif (defined(__arm64__) && defined(__APPLE__)) || defined(__aarch64__)
+    // sysctl -a: we read is: 128, in future we get this at cmake file.
+    // hw.cachelinesize: 128
+    128;
 #else
 #error "cache_line_size not defined for this architecture"
 #endif
