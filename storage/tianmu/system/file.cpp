48b01b4d8be1e89d4144abb7de712209930cada0
CodingSuen <xavier.suen@outlook.com>
2022-10-21 14:57:24 +0800

    fix(tianmu):  feature: The error display is too general, and the cause of the error cannot be clearly located #706

--- storage/tianmu/system/file.cpp
+++ storage/tianmu/system/file.cpp
@@ -27,7 +27,9 @@ int TianmuFile::Open(std::string const &file, int flags, mode_t mode) {
   name_ = file;
 
   fd_ = open(file.c_str(), flags, mode);
-  if (fd_ == -1) ThrowError(errno);
+  if (fd_ == -1) 
+    throw common::TIANMUError(common::ErrorCode::FAILED, std::to_string(errno) + " " + strerror(errno));
+    // ThrowError(errno);
   return fd_;
 }
 
