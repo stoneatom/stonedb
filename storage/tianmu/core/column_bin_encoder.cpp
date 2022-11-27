6ddac32ed71af6fc65f9c1fe855969d6f3ab711d
zhx <15150573650@163.com>
2022-11-27 18:28:40 +0800

    fix the code format

--- storage/tianmu/core/column_bin_encoder.cpp
+++ storage/tianmu/core/column_bin_encoder.cpp
@@ -300,7 +300,7 @@ bool ColumnBinEncoder::EncoderInt::SecondColumn(vcolumn::VirtualColumn *vc) {
   }
   bool is_timestamp1 = (this->vc_type.GetTypeName() == common::CT::TIMESTAMP);
   bool is_timestamp2 = (vc->Type().GetTypeName() == common::CT::TIMESTAMP);
-  if (is_timestamp1 ^ is_timestamp2 )
+  if (is_timestamp1 ^ is_timestamp2)
     return false;  // cannot compare datetime with anything different than
                    // timestamp
   // Easy case: integers/decimals with the same precision
