55b3544df3920bff1fba487c2dcd11c96b7a4a98
fuxiang <duanfuxiang0@gmail.com>
2023-02-15 16:28:27 +0800

    reback stone_build

--- scripts/stonedb_build.sh
+++ scripts/stonedb_build.sh
@@ -18,7 +18,7 @@ build_time=`date "+%Y-%m-%d_%H-%M-%S"`
 build_tag=build_${branch}_${cpu_arc}_${os_dist}${os_dist_release}_${build_type}_${build_time}
 build_log=`pwd`/../build/build_log_${build_tag}.log
 
-install_target=/stonedb57/install
+install_target=`pwd`/../build/install
 
 # step 2. shutdown server first
 if test -f ${install_target}/support-files/mysql.server
@@ -52,13 +52,12 @@ cmake ../../ \
 -DDEFAULT_CHARSET=utf8mb4 \
 -DDEFAULT_COLLATION=utf8mb4_general_ci \
 -DDOWNLOAD_BOOST=0 \
--DDOWNLOAD_ROCKSDB=0 \
--DWITH_ROCKSDB=/usr/local/stonedb-gcc-rocksdb \
 -DWITH_BOOST=/usr/local/stonedb-boost \
 -DWITH_MARISA=/usr/local/stonedb-marisa \
+-DWITH_ROCKSDB=/usr/local/stonedb-gcc-rocksdb \
 2>&1 | tee -a ${build_log}
 
-# make
+# step 5. make & make install
 make VERBOSE=1 -j`nproc`                                             2>&1 | tee -a ${build_log}
 make install                                                         2>&1 | tee -a ${build_log}
-echo "current dir is `pwd`"                                          2>&1 | tee -a ${build_log}
+echo "current dir is `pwd`"                                                              2>&1 | tee -a ${build_log}
