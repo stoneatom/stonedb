98d6a92276a794cb1d0abb89b4f333a43572ed79
LiMK <slibre@qq.com>
2023-03-30 17:45:49 +0800

    feat: add Baidu statistics script(#1498)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -255,6 +255,17 @@ const config = {
       },
     ],
   ],
+  scripts:[
+    {
+      script:`var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?7bd24ad933c7b555361696ce01e3e8ff";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();`
+    }
+  ]
 };
 
 module.exports = config;
