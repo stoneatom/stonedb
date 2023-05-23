da4d7595bf126ea60f09ea5a135d3122edc6fd1f
hustjieke <gaoriyao1@gmail.com>
2023-05-23 06:18:42 +0000

    feat(tianmu): limit unsigned range #1716

--- storage/tianmu/common/common_definitions.cpp
+++ storage/tianmu/common/common_definitions.cpp
@@ -32,6 +32,76 @@ void PushWarning(THD *thd, Sql_condition::enum_severity_level level, uint code, 
   push_warning(thd, level, code, msg);
 }
 
+// Here for args int `type`, we do not use enum directly as it'll caused compiling failed for the dependent package.
+void PushWarningIfOutOfRange(THD *thd, std::string col_name, int64_t v, int type, bool unsigned_flag) {
+  // below `0` is for min unsigned value.
+  switch (type) {
+    case 1: {  // MYSQL_TYPE_TINY
+      if (unsigned_flag && (static_cast<uint64_t>(v) > TIANMU_TINYINT_MAX)) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, 0, TIANMU_TINYINT_MAX, unsigned_flag, v).c_str());
+        throw std::exception();
+      } else if (v > TIANMU_TINYINT_MAX || v < TIANMU_TINYINT_MIN) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, TIANMU_TINYINT_MIN, TIANMU_TINYINT_MAX, unsigned_flag, v).c_str());
+        throw std::exception();
+      }
+    } break;
+    case 2: {  // MYSQL_TYPE_SHORT
+      if (unsigned_flag && (static_cast<uint64_t>(v) > TIANMU_SMALLINT_MAX)) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, 0, TIANMU_SMALLINT_MAX, unsigned_flag, v).c_str());
+      } else if (v > TIANMU_SMALLINT_MAX || v < TIANMU_SMALLINT_MIN) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, TIANMU_SMALLINT_MIN, TIANMU_SMALLINT_MAX, unsigned_flag, v).c_str());
+      };
+    } break;
+    case 9: {  // MYSQL_TYPE_INT24
+      if (unsigned_flag && (static_cast<uint64_t>(v) > TIANMU_MEDIUMINT_MAX)) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, 0, TIANMU_MEDIUMINT_MAX, unsigned_flag, v).c_str());
+      } else if (v > TIANMU_MEDIUMINT_MAX || v < TIANMU_MEDIUMINT_MIN) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, TIANMU_MEDIUMINT_MIN, TIANMU_MEDIUMINT_MAX, unsigned_flag, v).c_str());
+      }
+    } break;
+    case 3: {  // MYSQL_TYPE_LONG
+      if (unsigned_flag && (static_cast<uint64_t>(v) > TIANMU_INT_MAX)) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, 0, TIANMU_INT_MAX, unsigned_flag, v).c_str());
+      } else if (v > TIANMU_INT_MAX || v < TIANMU_INT_MIN) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, TIANMU_INT_MIN, TIANMU_INT_MAX, unsigned_flag, v).c_str());
+      }
+    } break;
+    case 8: {  // MYSQL_TYPE_LONGLONG
+      if (unsigned_flag && (static_cast<uint64_t>(v) > TIANMU_BIGINT_MAX)) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, 0, TIANMU_BIGINT_MAX, unsigned_flag, v).c_str());
+      } else if (v > TIANMU_BIGINT_MAX || v < TIANMU_BIGINT_MIN) {
+        PushWarning(thd, Sql_condition::SL_WARNING, ER_WARN_DATA_OUT_OF_RANGE,
+                    getErrMsg(col_name, TIANMU_BIGINT_MIN, TIANMU_BIGINT_MAX, unsigned_flag, v).c_str());
+      }
+    } break;
+    default:  // For type which is not integer, nothing to do
+      break;
+  }
+}
+
+// Msg: Out of range[min, max] for column 'col' value: 123
+// Just for unsigned type check in tianmu engine.
+std::string getErrMsg(std::string col_name, int64_t min, int64_t max, bool unsigned_flag, int64_t v) {
+  std::string str = "Out of range[";
+  str += std::to_string(min);
+  str += ", ";
+  str += std::to_string(max);
+  str += "] for column '";
+  str += col_name;
+  str += "' value: ";
+  str += unsigned_flag ? std::to_string(static_cast<uint64_t>(v)) : std::to_string(v);
+  return str;
+}
+
 std::string TX_ID::ToString() const {
   std::stringstream ss;
   ss << std::setfill('0') << std::setw(sizeof(v) * 2) << std::hex << v;
